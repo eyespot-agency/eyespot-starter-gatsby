725984e7c1aa430a869893ac80241646dbd1180f
raed_emiketic <raed.fadhlaoui@emiketic.com>
2021-06-11 10:39:08 +0100

    feat: Handle Gatsby GraphQL queries

--- src/pages/sample-page.js
+++ src/pages/sample-page.js
@@ -9,6 +9,9 @@ import { Link } from 'gatsby';
 // UI lib components
 import { Container } from 'react-grid-system';
 
+// Static Image
+import { StaticImage } from 'gatsby-plugin-image';
+
 // Page wrappers
 import Layout from '../shared/PageLayout';
 import Seo from '../shared/Seo';
@@ -27,6 +30,16 @@ function SamplePage() {
       <Container fluid>
         <h1>Hi from the second page</h1>
         <p>Welcome to page 2</p>
+
+        <StaticImage
+          src="../images/gatsby-astronaut.png"
+          alt="A kitten"
+          placeholder="blurred"
+          layout="fixed"
+          width={300}
+          height={500}
+        />
+
         <SampleComponent />
         <Link to="/">Go back to the homepage</Link>
       </Container>

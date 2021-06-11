1a53314fd8e878731575999a76e0f579833bf2bb
raed_emiketic <raed.fadhlaoui@emiketic.com>
2021-06-11 12:53:24 +0100

    fix: Reset configuration

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 set -xeo pipefail
 
-BUILD=$1
 
-if [ -z "$BUILD" ] || [ ! -d "$BUILD" ] ; then
+TARGET=$1
+
+if [ -z "$TARGET" ] || [ ! -d "$TARGET" ] ; then
   exit 1
 fi
 
-if [ -n "$DEPLOY_PATH" ] && [ -d "$DEPLOY_PATH" ]; then
-  rsync -rlzv "$BUILD" "$DEPLOY_PATH"
-
+if [ -n "$PREVIEW_FTP_HOST" ] && [ "$PREVIEW_FTP_PATH" ] && [ -n "$PREVIEW_FTP_USERNAME" ] && [ -n "$PREVIEW_FTP_PASSWORD" ]; then
+  cd $TARGET
+  ncftpput -mR -v -u "$PREVIEW_FTP_USERNAME" -p "$PREVIEW_FTP_PASSWORD" "$PREVIEW_FTP_HOST" "$PREVIEW_FTP_PATH" ./
 else
   exit 1
 fi
